415d87665d0045ba75d8ce351ebb33d8c42c6026
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-05-09 22:46:05 +0200

    z

---
+++ ApiJuegos/Models/leer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiJuegos.Models
+{
+    public class leer
+    {//borrame Falta Meter nugets. Ver docs 18,19,24. 
+       // adminsql Tajamar123
+
+    }
+}
